f5dd4ec559998c66bf79f98af644e5704ca23328
misu009 <mihaillivadaru2@gmail.com>
2024-05-12 17:41:22 +0300

    final commmit

--- js/script.js
+++ js/script.js
@@ -1,16 +1,16 @@
 function startCountdown() {
-let count = 5;
-const countdownElement = document.getElementById("countdown");
+    let count = 5;
+    const countdownElement = document.getElementById("countdown");
 
-const countdownInterval = setInterval(() => {
-    countdownElement.textContent = count;
-    count--;
+    const countdownInterval = setInterval(() => {
+        countdownElement.textContent = count;
+        count--;
 
-    if (count < 0) {
-    clearInterval(countdownInterval);
-    window.location.href = "../html/index.html";
-    }
-}, 1000);
+        if (count < 0) {
+            clearInterval(countdownInterval);
+            window.location.href = "../html/index.html";
+        }
+    }, 1000);
 }
 
 // Start countdown when the page loads
